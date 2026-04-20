88d5c018f6c7973cbf386907600a5d70920acc01
Faridin Islam <faridin05saif@gmail.com>
2026-04-20 14:30:58 +0600

    remove sd, sa

--- app.py
+++ app.py
@@ -946,7 +946,9 @@ def register():
     segments = list(
         segments_collection.find(
             {'_id': {'$nin': [
-                ObjectId('6996cf26e7eb96d29e2010c6')
+                ObjectId('6996cf26e7eb96d29e2010c6'),
+                ObjectId('6996cf26e7eb96d29e2010c8'),
+                ObjectId('6996cf26e7eb96d29e2010cc')
             ]}},
             {'_id': 1, 'name': 1, 'price': 1, 'categories': 1}
         ))
